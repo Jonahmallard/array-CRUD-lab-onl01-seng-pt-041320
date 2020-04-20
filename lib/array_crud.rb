aaecd33ef983b772eec54a262ff058e06728ac50
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-20 03:06:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array<<"arrays!" 
+  create_an_array.push("arrays!") 
+  puts create_an_array
 end
 
 def add_element_to_start_of_array(array, element)
