00be6fa1395a28c2dca5133680bc33219a4a3d8c
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-20 03:11:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["cats", "dogs", "fish", "alligators",]
+  create_an_array = ["cats", "dogs", "fish", "alligators",]
 end
 
 def add_element_to_end_of_array(array, element)
